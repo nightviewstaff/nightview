82dcf144b30f0c4d665ed180fa9334bd81770a90
Magnompower <143427171+Magnompower@users.noreply.github.com>
2024-12-21 02:02:34 +0100

    Attempt to refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:nightview/api/firebase_api.dart';
 import 'package:nightview/firebase_options.dart';
-import 'package:nightview/firestore/firestore_updater.dart';
-import 'package:nightview/models/chat_subscriber.dart';
-import 'package:nightview/models/search_new_chat_helper.dart';
-import 'package:nightview/models/search_friends_helper.dart';
+import 'package:nightview/services/firestore/firestore_updater.dart';
+import 'package:nightview/helpers/chat_subscriber.dart';
+import 'package:nightview/helpers/users/chats/search_new_chat_helper.dart';
+import 'package:nightview/helpers/users/friends/search_friends_helper.dart';
 import 'package:nightview/providers/balladefabrikken_provider.dart';
 import 'package:nightview/providers/global_provider.dart';
 import 'package:nightview/providers/login_registration_provider.dart';
@@ -36,7 +36,7 @@ import 'package:nightview/screens/night_map/night_map_main_offer_screen.dart';
 import 'package:nightview/screens/night_social/find_new_friends_screen.dart';
 import 'package:nightview/screens/night_social/friend_requests_screen.dart';
 import 'package:nightview/screens/night_social/night_social_conversation_screen.dart';
-import 'package:nightview/screens/preferences/preferences_main_screen.dart';
+// import 'package:nightview/screens/preferences/preferences_main_screen.dart';
 import 'package:nightview/screens/profile/other_profile_main_screen.dart';
 import 'package:nightview/screens/swipe/swipe_main_screen.dart';
 import 'package:nightview/screens/utility/waiting_for_login_screen.dart';
@@ -44,7 +44,8 @@ import 'package:provider/provider.dart';
 
 import 'constants/Initializator.dart';
 import 'constants/colors.dart';
-import 'firestore/add_club.dart';
+import 'services/firestore/add_club.dart';
+import 'notifications/notification_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -55,7 +56,12 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  await FirebaseApi().initNotifications();
+
+  final notificationManager = NotificationManager();
+  await notificationManager.initLocalNotifications();
+  notificationManager.scheduleWeeklyNotifications();
+
+
 
   // FirestoreUpdater firestoreUpdater = FirestoreUpdater();
   // firestoreUpdater.updateFirestoreData(); // Updates Firestore.
@@ -63,7 +69,30 @@ void main() async {
   Initializator initializator = Initializator(); // Rename
   initializator.initializeNeededTasks();
 
-   // AddClub addClub = AddClub(); addClub.addSpecificClub1(); // To add each new club manually.
+   AddClub addClub = AddClub();
+   // addClub.addSpecificClub72(); // To add each new club manually.
+   // addClub.addSpecificClub73(); // To add each new club manually.
+   // addClub.addSpecificClub74(); // To add each new club manually.
+   // addClub.addSpecificClub75(); // To add each new club manually.
+   //  addClub.addSpecificClub76(); // To add each new club manually.
+   //  addClub.addSpecificClub77(); // To add each new club manually.
+   //  addClub.addSpecificClub78(); // To add each new club manually.
+   //  addClub.addSpecificClub79(); // To add each new club manually.
+   //  addClub.addSpecificClub80(); // To add each new club manually.
+   //  addClub.addSpecificClub81(); // To add each new club manually.
+   //  addClub.addSpecificClub82(); // To add each new club manually.
+   //  addClub.addSpecificClub84(); // To add each new club manually.
+   //  addClub.addSpecificClub85(); // To add each new club manually.
+   //  addClub.addSpecificClub86(); // To add each new club manually.
+   //  addClub.addSpecificClub87(); // To add each new club manually.
+   //  addClub.addSpecificClub88(); // To add each new club manually.
+   //  addClub.addSpecificClub89(); // To add each new club manually.
+   //  addClub.addSpecificClub90(); // To add each new club manually.
+   //  addClub.addSpecificClub100(); // To add each new club manually.
+   //  addClub.addSpecificClub99(); // To add each new club manually.
+   //  addClub.addSpecificClub98(); // To add each new club manually.
+   //  addClub.addSpecificClub97(); // To add each new club manually.
+   //  addClub.addSpecificClub96(); // To add each new club manually.
 
   runApp(NightViewApp());
 
@@ -124,7 +153,7 @@ class NightViewApp extends StatelessWidget {
           MyProfileMainScreen.id: (context) => const MyProfileMainScreen(),
           NightSocialConversationScreen.id: (context) => const NightSocialConversationScreen(),
           MainScreen.id: (context) => const MainScreen(),
-          PreferencesMainScreen.id: (context) => const PreferencesMainScreen(),
+          // PreferencesMainScreen.id: (context) => const PreferencesMainScreen(),
           SwipeMainScreen.id: (context) => const SwipeMainScreen(),
           WaitingForLoginScreen.id: (context) => const WaitingForLoginScreen(),
           NightMapMainOfferScreen.id: (context) => const NightMapMainOfferScreen(),
@@ -144,5 +173,6 @@ class NightViewApp extends StatelessWidget {
       ),
     );
   }
-
 }
+
+
