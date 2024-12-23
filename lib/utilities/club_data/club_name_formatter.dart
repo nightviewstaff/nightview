675626c1d24063f77e52f5593fea65d71f922d53
Magnompower <143427171+Magnompower@users.noreply.github.com>
2024-12-23 01:20:57 +0100

    Updated class

--- lib/utilities/club_data/club_name_formatter.dart
+++ lib/utilities/club_data/club_name_formatter.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 import '../../models/clubs/club_data.dart';
 import 'club_data_location_formatting.dart';
 import 'club_distance_calculator.dart';
@@ -7,58 +9,17 @@ class ClubNameFormatter {
   //TODO Display everything in here properly
 
   //TODO If same club say place (Irish pub frederiksberg)
-  static String displayClubName(ClubData club){
+  static String displayClubName(ClubData club) {
     return formatClubName(club.name);
   }
+
+  // static Text displayClubNameText(ClubData club){
+  // }
 
   //TODO big small location text
-  static String displayClubLocation(ClubData club){
-    return ClubDataLocationFormatting.determineLocationFromCoordinates(club.lat, club.lon);
-  }
-
-  static String displayDistanceToClub({
-    required double userLat,
-    required double userLon,
-    required ClubData club,
-  }) {
-    return _formatDistance(
-      ClubDistanceCalculator.calculateDistance(
-        lat1: userLat,
-        lon1: userLon,
-        lat2: club.lat,
-        lon2: club.lon,
-      ),
-    );
-  }
-
-
-  static String displayClubNameWithLocationAndDistance({
-    required ClubData club,
-    required double userLat,
-    required double userLon,
-  }) {
-    String formattedName = formatClubName(club.name);
-    String location =
-        ClubDataLocationFormatting.determineLocationFromCoordinates(
-            club.lat, club.lon);
-    double distance = ClubDistanceCalculator.calculateDistance(
-      lat1: userLat,
-      lon1: userLon,
-      lat2: club.lat,
-      lon2: club.lon,
-    );
-
-    // Convert distance to meters if it's less than 1 km
-    String distanceText = distance < 1
-        ? "${(distance * 1000).toStringAsFixed(0)} m"
-        : "${distance.toStringAsFixed(1)} km";
-
-    return "$formattedName ($distanceText)\n($location)"; // TODO Want distanceText pushed right
-  }
-
-  static String displayClubNameFormatted(ClubData club) {
-    // Redundant to have a method that returns another method?
-    return formatClubName(club.name);
+  static String displayClubLocation(ClubData club) {
+    return ClubDataLocationFormatting.determineLocationFromCoordinates(
+        club.lat, club.lon);
   }
 
   static String formatClubName(String clubName) {
@@ -77,10 +38,5 @@ class ClubNameFormatter {
     //TODO Make exceptions for specific names like 'BAR', 'KB3' and so on.
   }
 
-  static String _formatDistance(double distance) {
-    //TODO redund
-    return distance < 1
-        ? "${(distance * 1000).toStringAsFixed(0)} m"
-        : "${distance.toStringAsFixed(1)} km";
-  }
+
 }
