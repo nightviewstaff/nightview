0b04929c962d39ea844d1e2ef934bec01ab40cdd
Magnompower <143427171+Magnompower@users.noreply.github.com>
2024-12-23 01:21:33 +0100

    Added

---
+++ lib/locations/location_service.dart
@@ -0,0 +1,30 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
+
+class LocationService {
+  static Future<LatLng?> getUserLocation() async {
+    //TODO Move so can acess from anywhere
+    // Move
+    try {
+      // Check location permissions
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.deniedForever ||
+            permission == LocationPermission.denied) {
+          throw Exception('Location permissions are denied');
+        }
+      }
+
+      // Get the user's current location
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      // Convert Position to LatLng
+      return LatLng(position.latitude, position.longitude);
+    } catch (e) {
+      return null; // Return null if there's an error
+    }
+  }
+}
